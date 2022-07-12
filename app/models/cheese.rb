519a76cbf65a8a8673b445f24f075b3943cc0a0e
JawaraGordon <jawaragordon@icloud.com>
2022-07-12 13:53:22 -0400

    completed assignment

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -4,4 +4,15 @@ class Cheese < ApplicationRecord
     "#{name}: $#{price}"
   end
 
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   if cheese
+  #     render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  #   else
+  #     # will produce a 404 status code
+  #     status: :not_found 
+  #     render json: { error: 'Cheese not found' }, status: :not_found
+  #   end
+  # end
+
 end
